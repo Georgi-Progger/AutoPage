c9ded379acf8374468638d0f060962d3971fcb6a
Georgi-Progger <hugaeag@gmail.com>
2023-05-03 18:18:29 +0300

    update

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import MainPhoto from "../imgs/photo.png"
+import React , { useState } from 'react';
 import "./MainPage.css"
 const MainPage : React.FC = () => {
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  }
+
+  const closePopup = () => {
+    setShowPopup(false);
+  }
+
   return (
     <div className='main'>
+       <div className='CallWindow' onClick={togglePopup}>
+        <div className='CallIcon'></div>
+          Позвонить
+        </div>
+        {showPopup && 
+          <div>
+            <div className="PopupOverlay" onClick={closePopup}></div>
+            <div className="Popup">
+              <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+              </ul>  
+              <div className='CallPop'>
+                Позвонить
+              </div>
+            </div>
+          </div>
+        }
       <div className='MainPhoto'>
         <div className='TextMain'>
           <p>Лёгкое начало<br/>
@@ -14,13 +62,22 @@ const MainPage : React.FC = () => {
         <div className='Info'>
         <div className='InfoMain'>
           <div className='Stick'></div>
-          <p>Выкуп. Продажа.<br/> Трейд-ин</p>
+          <div className='InfoMainText'>
+            <div className='InfoMainText-Inside'>
+              <div className='InfoIcon'>
+                <div className='InfoIcon-Inside'></div>
+              </div>
+              <p>Выкуп. Продажа.<br/> Трейд-ин</p>
+            </div>
+            <p>Выгодно выкупим любой автомобиль. 
+            Быстро продадим Ваш автомобиль. 
+            Легко обменяем старый на новый</p>
+          </div>
         </div>
-            <div className='InfoPhoto'></div>
-        </div>
-    </div>
-    
-  );
+          <div className='InfoPhoto'></div>
+      </div>
+  </div>
+);
 }
 
 export default MainPage;
