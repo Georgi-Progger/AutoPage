f113352d9a20ed9a7137f49db1ddd4f5b7b4568d
Georgi-Progger <hugaeag@gmail.com>
2023-05-04 03:13:44 +0300

    update adaptive

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -3,63 +3,69 @@ import "./MainPage.css"
 const MainPage : React.FC = () => {
 
   const [showPopup, setShowPopup] = useState(false);
+  const [isRadioChecked, setIsRadioChecked] = useState(false);
 
   const togglePopup = () => {
     setShowPopup(!showPopup);
-  }
+  };
 
   const closePopup = () => {
     setShowPopup(false);
-  }
+    setIsRadioChecked(false);
+  };
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsRadioChecked(event.target.checked);
+  };
 
   return (
     <div className='main'>
-       <div className='CallWindow' onClick={togglePopup}>
+      <div className='CallWindow' onClick={togglePopup}>
         <div className='CallIcon'></div>
-          Позвонить
-        </div>
-        {showPopup && 
-          <div>
-            <div className="PopupOverlay" onClick={closePopup}></div>
-            <div className="Popup">
-              <ul>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-              </ul>  
-              <div className='CallPop'>
-                Позвонить
-              </div>
+        <p>Позвонить</p>
+      </div>
+      {showPopup && (
+        <div>
+          <div className='PopupOverlay' onClick={closePopup}></div>
+          <div className='Popup'>
+            <ul>
+              <li>
+                <label htmlFor='option1'>Олег</label>
+                <input
+                  type='radio'
+                  id='option1'
+                  name='options'
+                  value='1'
+                  onChange={handleRadioChange}
+                />
+              </li>
+              <li>
+                <label htmlFor='option2'>Аслан</label>
+                <input
+                  type='radio'
+                  id='option2'
+                  name='options'
+                  value='2'
+                  onChange={handleRadioChange}
+                />
+              </li>
+            </ul>
+            <div className={`CallPop ${!isRadioChecked ? 'disabled' : ''}`}>
+              <div className='CallIcon'></div>
+              Позвонить
             </div>
           </div>
-        }
+        </div>
+      )}
       <div className='MainPhoto'>
         <div className='TextMain'>
-          <p>Лёгкое начало<br/>
-            пути <span>вместе</span> <br/>
-            с Алания - Авто</p>
+          <p>Лёгкое начало
+            пути <span>вместе </span> 
+            с Алания - Авто
+          </p>
         </div>
       </div>
-        <div className='Info'>
+      <div className='Info'>
         <div className='InfoMain'>
           <div className='Stick'></div>
           <div className='InfoMainText'>
@@ -73,8 +79,8 @@ const MainPage : React.FC = () => {
             Быстро продадим Ваш автомобиль. 
             Легко обменяем старый на новый</p>
           </div>
-        </div>
-          <div className='InfoPhoto'></div>
+          </div>
+        <div className='InfoPhoto'></div>
       </div>
   </div>
 );
